acbfa99a65b0f5ab474ebea1e6becb65cb3e7750
nedbitar <nabitar@ucsd.edu>
2024-06-04 13:29:00 -0700

    Update homePage.js

--- project/homePage/homePage.js
+++ project/homePage/homePage.js
@@ -49,10 +49,12 @@ function renderProjects() {
 
     const projectData = JSON.parse(localStorage.getItem('projectData')).project_data;
     for (const projectId in projectData) {
-        const project = projectData[projectId];
-        if (project.active) {
-            const projectElement = createProjectElement(project, projectId);
-            projectsList.appendChild(projectElement);
+        if (Object.prototype.hasOwnProperty.call(projectData, projectId)) {
+            const project = projectData[projectId];
+            if (project.active) {
+                const projectElement = createProjectElement(project, projectId);
+                projectsList.appendChild(projectElement);
+            }
         }
     }
 }
@@ -100,7 +102,7 @@ function createProjectElement(project, projectId) {
 
     projectElement.appendChild(projectActions);
     projectElement.addEventListener('click', () => {
-        const projectDataCopy = { ...JSON.parse(localStorage.getItem('projectData')) };
+        const projectDataCopy = JSON.parse(JSON.stringify(JSON.parse(localStorage.getItem('projectData'))));
         projectDataCopy.current_project = projectId;
         projectDataCopy.current_date = new Date().toLocaleDateString();
         localStorage.setItem('projectData', JSON.stringify(projectDataCopy));
@@ -112,7 +114,7 @@ function createProjectElement(project, projectId) {
 
 // Function to archive a project
 function archiveProject(projectId) {
-    const projectDataCopy = { ...JSON.parse(localStorage.getItem('projectData')) };
+    const projectDataCopy = JSON.parse(JSON.stringify(JSON.parse(localStorage.getItem('projectData'))));
     projectDataCopy.project_data[projectId].active = false;
     localStorage.setItem('projectData', JSON.stringify(projectDataCopy));
     renderProjects();
@@ -120,8 +122,8 @@ function archiveProject(projectId) {
 
 // Function to delete a project
 function deleteProject(projectId) {
-    const projectDataCopy = { ...JSON.parse(localStorage.getItem('projectData')) };
-    projectDataCopy.project_data[projectId] = undefined;
+    const projectDataCopy = JSON.parse(JSON.stringify(JSON.parse(localStorage.getItem('projectData'))));
+    delete projectDataCopy.project_data[projectId];
     localStorage.setItem('projectData', JSON.stringify(projectDataCopy));
     renderProjects();
 }
